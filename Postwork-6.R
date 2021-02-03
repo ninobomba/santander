c048f15ddf9bf45bec754444a0a782dc33fa0dbd
angyf <60585717+angyf@users.noreply.github.com>
2021-02-03 10:20:54 -0600

    Update Postwork-6.R

--- Postwork-6.R
+++ Postwork-6.R
@@ -12,28 +12,72 @@ match.data<-mutate(match.data,sumagoles=home.score+away.score,date=as.Date(match
 #agregamos columna con año y mes
 match.data <- mutate(match.data, Ym = format(date, "%Y-%m"))
 
+
 #Agregamos datos nulos para completar la frecuencia mensual de los datos
 #de los meses junio y julio
-ym<-c("2018-06","2018-07")
+
+ym<-c("2011-06","2011-07")
 promedio<-c(NA,NA)
 a<-data.frame(ym,promedio)
 colnames(a)<-c("Ym","promedios")
+ym<-c("2012-06","2012-07")
+promedio<-c(NA,NA)
+b<-data.frame(ym,promedio)
+colnames(b)<-c("Ym","promedios")
+ym<-c("2013-06","2013-07")
+promedio<-c(NA,NA)
+c<-data.frame(ym,promedio)
+colnames(c)<-c("Ym","promedios")
+ym<-c("2014-06","2014-07")
+promedio<-c(NA,NA)
+d<-data.frame(ym,promedio)
+colnames(d)<-c("Ym","promedios")
+ym<-c("2015-06","2015-07")
+promedio<-c(NA,NA)
+e<-data.frame(ym,promedio)
+colnames(e)<-c("Ym","promedios")
+ym<-c("2016-06","2016-07")
+promedio<-c(NA,NA)
+f<-data.frame(ym,promedio)
+colnames(f)<-c("Ym","promedios")
+ym<-c("2017-06","2017-07")
+promedio<-c(NA,NA)
+g<-data.frame(ym,promedio)
+colnames(g)<-c("Ym","promedios")
+ym<-c("2018-06","2018-07")
+promedio<-c(NA,NA)
+h<-data.frame(ym,promedio)
+colnames(h)<-c("Ym","promedios")
 ym1<-c("2019-06","2019-07")
-a1<-data.frame(ym1,promedio)
-colnames(a1)<-c("Ym","promedios")
+i<-data.frame(ym1,promedio)
+colnames(i)<-c("Ym","promedios")
 
 # data frame de promedios por mes 
 promedios<- match.data %>% group_by(Ym) %>% summarise(promedios = mean(sumagoles))
-b<-rbind(promedios%>%filter(Ym>="2017-08",Ym<="2018-05"),
-      a,
-      promedios%>%filter(Ym>="2018-08",Ym<="2019-05"),
-      a1,
-      promedios%>%filter(Ym>="2019-08",Ym<="2019-12"))
+j<-rbind(promedios%>%filter(Ym>="2010-08",Ym<="2011-05"),
+         a,
+         promedios%>%filter(Ym>="2011-08",Ym<="2012-05"),
+         b,
+         promedios%>%filter(Ym>="2012-08",Ym<="2013-05"),
+         c,
+         promedios%>%filter(Ym>="2013-08",Ym<="2014-05"),
+         d,
+         promedios%>%filter(Ym>="2014-08",Ym<="2015-05"),
+         e,
+         promedios%>%filter(Ym>="2015-08",Ym<="2016-05"),
+         f,
+         promedios%>%filter(Ym>="2016-08",Ym<="2017-05"),
+         g,
+         promedios%>%filter(Ym>="2017-08",Ym<="2018-05"),
+         h,
+         promedios%>%filter(Ym>="2018-08",Ym<="2019-05"),
+         i,
+         promedios%>%filter(Ym>="2019-08",Ym<="2019-12"))
 
-
+class(b)
 # Crea la serie de tiempo del promedio por mes de la suma de goles hasta diciembre de 2019.
-serie <- ts(b$promedios, st = c(2017, 8), # A partir de agosto 2017
-             frequency = 12,end=c(2019,12)) # Hasta diciembre de 2019
+serie <- ts(j$promedios, st = c(2010, 8), 
+            frequency = 12,end=c(2019,12)) # Hasta diciembre de 2019
 
 
 # Grafica la serie de tiempo.
